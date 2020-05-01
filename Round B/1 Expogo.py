dbae9f8c8fa82b2f46993c6ea7923b4b5f491688
Joseandres Hinojoza <57422356+Ualabi@users.noreply.github.com>
2020-05-01 17:02:05 -0500

    Update 1 Expogo.py

--- Round B/1 Expogo.py
+++ Round B/1 Expogo.py
@@ -16,11 +16,11 @@ def solve(x,y,h):
                 break
             if (abs(x)%2 != abs(t[0])%2):
                 to = t
-        res = solve(x, to[0], h+1)
-        if (res == "IMPOSSIBLE"):
-            return res
-        res = to[1] + res
-        return res
+        ans = solve(x, to[0], h+1)
+        if (ans == "IMPOSSIBLE"):
+            return ans
+        ans = to[1] + ans
+        return ans
     if (y%2 == 0):
         y = y//2
         can = [[(x-1)//2, 'E'], [(x+1)//2, 'W']]
@@ -30,15 +30,15 @@ def solve(x,y,h):
                 break
             if (abs(y) % 2 != abs(t[0]) % 2):
                 to = t
-        res = solve(to[0], y, h+1)
-        if (res == "IMPOSSIBLE"):
-            return res
-        res = to[1] + res
-        return res
+        ans = solve(to[0], y, h+1)
+        if (ans == "IMPOSSIBLE"):
+            return ans
+        ans = to[1] + ans
+        return ans
     return ""
 
 T = int(input())
 for i in range(T):
     j, k = map(int,input().split())
-    resp = solve(j,k,0)
-    print('Case #{}: {}'.format(i+1,resp))
+    ans = solve(j,k,0)
+    print('Case #{}: {}'.format(i+1,ans))
